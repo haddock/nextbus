3b0ba42ce1f96b75788c4264c5bc963c9a5f9c9f
haddock <bergljung@gmail.com>
2014-04-14 16:31:43 +0200

    enabled lein ring

--- project.clj
+++ project.clj
@@ -5,10 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.2"]
+                 [http-kit "2.1.16"]
                  [postgresql "9.1-901.jdbc4"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]]
   :min-lein-version "2.0.0"
   :main ^:skip-aot nextbus.web
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler nextbus.web/application}
   :profiles {:uberjar {:aot :all}})
