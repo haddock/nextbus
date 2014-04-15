f51ab86df8131c591ec290ae4d694e3e246cb5fd
haddock <bergljung@gmail.com>
2014-04-15 22:15:45 +0200

    hard wired one destination

--- src/nextbus/models/departure.clj
+++ src/nextbus/models/departure.clj
@@ -2,15 +2,11 @@
 	(:require [org.httpkit.client :as http]
 						[clojure.data.json :as json]))
 
-;;(defn all []
-;;	[{:id 1, :bus_nr "3", :from_station "Kungsholms Kyrka", :to_station "Skanstull", :estimated "nu", :created_at "2014-04-14"}
-;;	 {:id 2, :bus_nr "3", :from_station "Kungsholms Kyrka", :to_station "Skanstull", :estimated "om 3 minuter", :created_at "2014-04-14"}])
-
-(defn all []
-	(let [{:keys [status headers body error] :as resp} @(http/get "https://api.trafiklab.se/sl/realtid2/GetAllDepartureTypes.json/1204/10?key=HDefrbKfMrC0mdm1MEuoaziRZT4h01Kk")]
+(defn data-from-sl []
+	(let [{:keys [status headers body error] :as resp} @(http/get "https://api.trafiklab.se/sl/realtid2/GetAllDepartureTypes.json/1204/30?key=HDefrbKfMrC0mdm1MEuoaziRZT4h01Kk")]
   	(if error
     	(println "Failed, exception: " error)
-    	(println "Response: " resp))))
+    	body)))
 
-(defn departures-from-response [response]
-	(println (json/read-str response)))
+(defn all []
+	(group-by :Destination (:Buses (into {} (json/read-json (data-from-sl))))))
--- src/nextbus/views/departures.clj
+++ src/nextbus/views/departures.clj
@@ -3,17 +3,17 @@
 		[hiccup.core :refer [h]]))
 
 (defn display-departures [departures]
-	[:div {:class "departures"}
+	[:div {:class "panel panel-primary"}
+     [:div {:class "panel-heading"}
+       [:h3 {:class "panel-title"} "BUSS 3 MOT SÖDERSJUKHUSET"]]
+     	 [:div {:class "panel-body"}
+			   [:div {:class "departures"}
 	 (map
-	 	(fn [departure] [:h4 {:class "departure"} (h (:estimated departure))])
-	 	departures)])
+	 	(fn [departure] [:h3 {:class "departure"} (h (:DisplayTime departure))])
+	 	(get departures "Södersjukhuset"))]]])
 
 (defn index [departures]
 	(layout/common "NÄSTA BUSS"
 		[:div {:class "page-header"}
      [:h1 "NÄSTA BUSS FRÅN KUNGSHOLMS KYRKA"]]
-    [:div {:class "panel panel-primary"}
-     [:div {:class "panel-heading"}
-       [:h3 {:class "panel-title"} "BUSS 3 MOT SKANSTULL"]]
-     [:div {:class "panel-body"}
-		 (display-departures departures)]]))
+		 (display-departures departures)))
